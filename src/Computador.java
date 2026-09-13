cec94def80f93a6335c104e404448354d15a79d8
juazocar <juazocar@profesor.duoc.cl>
2026-09-13 10:31:11 -0300

    Primer commit

--- src/Computador.java
+++ src/Computador.java
@@ -2,4 +2,9 @@ public class Computador  extends Dispositivo{
     public Computador(String nombre, boolean activo) {
         super(nombre, activo);
     }
+
+    @Override
+    public void ejecutarDiagnostico() {
+        System.out.println("Verificando memoria y sistema del computador...");
+    }
 }
--- src/Impresora.java
+++ src/Impresora.java
@@ -2,4 +2,9 @@ public class Impresora  extends Dispositivo {
     public Impresora(String nombre, boolean activo) {
         super(nombre, activo);
     }
+
+    @Override
+    public void ejecutarDiagnostico() {
+        System.out.println("Comprobando tinta y papel de la impresora...");
+    }
 }
